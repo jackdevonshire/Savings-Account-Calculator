8f6c0e299427eeb0d078435c9d1f93932c1be5d8
Jack Devonshire <contact@jackdevonshire.co.uk>
2023-07-22 10:46:41 +0100

    Changing Interface for a base class
    A lot of the logic for depositing and withdrawing may be similar, but this will not always be the case. To avoid code duplication as much as possible, moving from an interface to a base class which contains the default implementation of the deposit and withdraw methods, as-well as the default finance summary which should never need to be altered regardless of the account. The CalculateFinance method is likely the only method that will be unique across all of the different account types.

---
+++ SavingsCalculator/SavingsCalculator/SavingsAccount/BaseSavingsAccount.cs
@@ -0,0 +1,96 @@
+using SavingsCalculator.Reports;
+
+namespace SavingsCalculator.SavingsAccount;
+
+public class BaseSavingsAccount
+{
+    protected string AccountName;
+    protected List<Transaction> Transactions;
+
+    protected BaseSavingsAccount(string accountName, DateOnly openingDate, double openingBalance)
+    {
+        AccountName = accountName;
+        Transactions = new List<Transaction>
+        {
+            new Transaction
+            {
+                Amount = openingBalance,
+                Date = openingDate,
+                Type = TransactionType.DEPOSIT
+            }
+        };
+    }
+
+    protected virtual void CalculateFinance()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual AccountSummary GetAccountSummary()
+    {
+        CalculateFinance();
+
+        var deposits = Transactions.Where(x => x.Type != TransactionType.WITHDRAW).Sum(x => x.Amount);
+        var withdrawals = Transactions.Where(x => x.Type == TransactionType.WITHDRAW).Sum(x => x.Amount);
+        var finalBalance = deposits - withdrawals;
+
+        var totalInterestAndBenefits = Transactions
+            .Where(x => x.Type is TransactionType.INTEREST or TransactionType.OTHER_BENEFIT).Sum(x => x.Amount);
+
+        return new AccountSummary
+        {
+            Account = AccountName,
+            DateFrom = Transactions.First().Date,
+            DateTo = Transactions.Last().Date,
+            Transactions = Transactions,
+            FinalBalance = finalBalance,
+            TotalCumulativeInterestAndBenefits = totalInterestAndBenefits
+        };
+    }
+    
+    public virtual BaseSavingsAccount Deposit(DateOnly date, double amount)
+    {
+        Transactions.Add(new Transaction
+        {
+            Date = date,
+            Amount = amount,
+            Type = TransactionType.DEPOSIT
+        });
+
+        return this;
+    }
+    
+    public virtual BaseSavingsAccount Withdraw(DateOnly date, double amount)
+    {
+        Transactions.Add(new Transaction
+        {
+            Date = date,
+            Amount = amount,
+            Type = TransactionType.DEPOSIT
+        });
+        
+        return this;
+    }
+
+    public virtual BaseSavingsAccount SetupMonthlyDeposit(DateOnly dateFrom, DateOnly dateTo, int dayOfMonth, double amount)
+    {
+        if (dayOfMonth is < 1 or > 31)
+            throw new Exception("Invalid day of the month provided");
+        
+        DateOnly currentDate = dateFrom;
+        
+        while (currentDate <= dateTo)
+        {
+            Transactions.Add(new Transaction
+            {
+                Date = new DateOnly(currentDate.Year, currentDate.Month, dayOfMonth),
+                Amount = amount,
+                Type = TransactionType.DEPOSIT
+            });
+            
+            currentDate = currentDate.AddMonths(1);
+        }
+        
+        return this;
+    }
+}
--- SavingsCalculator/SavingsCalculator/SavingsAccount/InstantAccessAccount.cs
+++ SavingsCalculator/SavingsCalculator/SavingsAccount/InstantAccessAccount.cs
@@ -2,10 +2,8 @@ using SavingsCalculator.Reports;
 
 namespace SavingsCalculator.SavingsAccount;
 
-public class InstantAccessAccount : ISavingsAccount
+public class InstantAccessAccount : BaseSavingsAccount
 {
-    private List<Transaction> _transactions;
-    private string _accountName;
     private double _annualEquivalentRateAsPercentage;
 
     public InstantAccessAccount(
@@ -13,38 +11,28 @@ public class InstantAccessAccount : ISavingsAccount
         DateOnly openingDate,
         double openingBalance, 
         double annualEquivalentRateAsPercentage
-        )
+) : base(accountName, openingDate, openingBalance)
     {
         _annualEquivalentRateAsPercentage = annualEquivalentRateAsPercentage;
-        _accountName = accountName;
-        _transactions = new List<Transaction>
-        {
-            new Transaction
-            {
-                Amount = openingBalance,
-                Date = openingDate,
-                Type = TransactionType.DEPOSIT
-            }
-        };
     }
-    
-    private void CalculateFinance()
+
+    protected override void CalculateFinance()
     {
         // First clear current transaction log of interest and benefit payments, and order by date ascending
-        _transactions = _transactions.Where(x => x.Type is TransactionType.DEPOSIT or TransactionType.WITHDRAW).ToList();
-        _transactions = _transactions.OrderBy(x => x.Date).ToList();
+        Transactions = Transactions.Where(x => x.Type is TransactionType.DEPOSIT or TransactionType.WITHDRAW).ToList();
+        Transactions = Transactions.OrderBy(x => x.Date).ToList();
 
         var interestAndBenefitsTransactions = new List<Transaction>();
         // Now loop through months and years etc
-        var dateFrom = _transactions.First().Date;
-        var dateTo = _transactions.Last().Date;
+        var dateFrom = Transactions.First().Date;
+        var dateTo = Transactions.Last().Date;
         
         var currentDate = dateFrom;
         double totalBalance = 0;
         while (currentDate <= dateTo)
         {
             // Here savings account specific calculation logic goes
-            var transactionsForMonth = _transactions
+            var transactionsForMonth = Transactions
                 .Where(x => x.Date.Month == currentDate.Month && x.Date.Year == currentDate.Year)
                 .OrderBy(x => x.Date)
                 .ToList();
@@ -56,7 +44,7 @@ public class InstantAccessAccount : ISavingsAccount
             var interestForMonth = (totalBalance + balanceForMonth) * ((_annualEquivalentRateAsPercentage / 100) / 12);
 
             totalBalance += balanceForMonth + interestForMonth;
-            _transactions.Add(new Transaction
+            Transactions.Add(new Transaction
             {
                 Type = TransactionType.INTEREST,
                 Date = new DateOnly(currentDate.Year, currentDate.Month, 28),
@@ -67,73 +55,6 @@ public class InstantAccessAccount : ISavingsAccount
         }
 
         // Finally add new interest and benefits transactions
-        _transactions = _transactions.OrderBy(x => x.Date).ToList();
-    }
-
-    public AccountSummary GetAccountSummary()
-    {
-        CalculateFinance();
-
-        var deposits = _transactions.Where(x => x.Type != TransactionType.WITHDRAW).Sum(x => x.Amount);
-        var withdrawals = _transactions.Where(x => x.Type == TransactionType.WITHDRAW).Sum(x => x.Amount);
-        var finalBalance = deposits - withdrawals;
-
-        var totalInterestAndBenefits = _transactions
-            .Where(x => x.Type is TransactionType.INTEREST or TransactionType.OTHER_BENEFIT).Sum(x => x.Amount);
-
-        return new AccountSummary
-        {
-            DateFrom = _transactions.First().Date,
-            DateTo = _transactions.Last().Date,
-            Transactions = _transactions,
-            FinalBalance = finalBalance,
-            TotalCumulativeInterestAndBenefits = totalInterestAndBenefits
-        };
-    }
-
-    public virtual ISavingsAccount Deposit(DateOnly date, double amount)
-    {
-        _transactions.Add(new Transaction
-        {
-            Date = date,
-            Amount = amount,
-            Type = TransactionType.DEPOSIT
-        });
-
-        return this;
-    }
-    
-    public virtual ISavingsAccount Withdraw(DateOnly date, double amount)
-    {
-        _transactions.Add(new Transaction
-        {
-            Date = date,
-            Amount = amount,
-            Type = TransactionType.DEPOSIT
-        });
-        
-        return this;
-    }
-
-    public virtual ISavingsAccount SetupMonthlyDeposit(DateOnly dateFrom, DateOnly dateTo, int dayOfMonth, double amount)
-    {
-        if (dayOfMonth is < 1 or > 31)
-            throw new Exception("Invalid day of the month provided");
-        
-        DateOnly currentDate = dateFrom;
-        
-        while (currentDate <= dateTo)
-        {
-            _transactions.Add(new Transaction
-                {
-                    Date = new DateOnly(currentDate.Year, currentDate.Month, dayOfMonth),
-                    Amount = amount,
-                    Type = TransactionType.DEPOSIT
-                });
-            
-            currentDate = currentDate.AddMonths(1);
-        }
-        
-        return this;
+        Transactions = Transactions.OrderBy(x => x.Date).ToList();
     }
 }
