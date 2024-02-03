cf9a0c3847616fd2103ff5b14f9d0de5993edc3c
Jack Devonshire <contact@jackdevonshire.co.uk>
2024-02-03 17:57:15 +0000

    Fixing Interest for LISA

--- SavingsCalculator/SavingsCalculator/SavingsAccount/LifetimeIsaAccount.cs
+++ SavingsCalculator/SavingsCalculator/SavingsAccount/LifetimeIsaAccount.cs
@@ -6,19 +6,25 @@ namespace SavingsCalculator.SavingsAccount;
 
 public class LifetimeIsaAccount : BaseSavingsAccount
 {
-    private readonly double _annualEquivalentRateAsPercentage;
+    private readonly InterestPaidType _interestPaidType;
+    private readonly CompoundType _compoundType;
+    private readonly double _actualInterestRate;
 
     public LifetimeIsaAccount(
         string accountName, 
         DateOnly openingDate,
         double openingBalance, 
-        double annualEquivalentRateAsPercentage
+        double annualEquivalentRateAsPercentage,
+        InterestPaidType interestPaidType = InterestPaidType.Monthly,
+        CompoundType compoundType = CompoundType.Annually
     ) : base(accountName, openingDate, openingBalance)
     {
-        _annualEquivalentRateAsPercentage = annualEquivalentRateAsPercentage;
+        _interestPaidType = interestPaidType;
+        _compoundType = compoundType;
+        _actualInterestRate = InterestAndTaxHelper.GetActualInterestRate(_interestPaidType, annualEquivalentRateAsPercentage);
     }
     
-    public BonusPeriod GetBonusPeriodForDate(DateOnly date)
+    public BonusPeriod GetBonusPeriodForDate(DateOnly currentDate)
     {
         /*
          * Bonuses are calculated based on deposits within a certain bonus period,
@@ -26,13 +32,13 @@ public class LifetimeIsaAccount : BaseSavingsAccount
          * bonus period for the current date.
          */
         
-        DateOnly startOfBonusPeriod = new DateOnly(date.Year, date.Month, 6);
-        DateOnly endOfBonusPeriod = new DateOnly(date.Year, date.Month, 5).AddMonths(1);
+        DateOnly startOfBonusPeriod = new DateOnly(currentDate.Year, currentDate.Month, 6);
+        DateOnly endOfBonusPeriod = new DateOnly(currentDate.Year, currentDate.Month, 5).AddMonths(1);
 
-        if (date < startOfBonusPeriod)
+        if (currentDate < startOfBonusPeriod)
         {
-            startOfBonusPeriod = new DateOnly(date.Year, date.Month - 1, 6);
-            endOfBonusPeriod = new DateOnly(date.Year, date.Month, 5);
+            startOfBonusPeriod = new DateOnly(currentDate.Year, currentDate.Month, 5).AddMonths(-1);
+            endOfBonusPeriod = new DateOnly(currentDate.Year, currentDate.Month, 5);
         }
         return new BonusPeriod()
         {
@@ -63,107 +69,109 @@ public class LifetimeIsaAccount : BaseSavingsAccount
     protected override void CalculateFinance(DateOnly? dateTo)
     {
         // First clear current transaction log of interest and benefit payments, and order by date ascending
-        Transactions = Transactions.Where(x => x.Type is 
-            TransactionType.Deposit or 
-            TransactionType.Withdraw or 
-            TransactionType.Penalty
-        ).ToList();
-        Transactions = Transactions.OrderBy(x => x.Date).ToList();
-
-        /* Get the starting month so we know when to pay interest into account for it to compound
-         * Based on the AER, interest should roughly be paid on the account birthday. Whilst not
-         * entirely accurate, it's a very good estimation and is how most online calculators appear to
-         * be doing it. This only really has an impact when we start seeing large, large sums of money -
-         * and in that case you should probably be seeing a financial advisor, and not using a random
-         * program found on Github.
-         */
-        var payInterestOn = Transactions.First().Date.AddYears(1);
+        Transactions = Transactions
+            .Where(x => x.Type is TransactionType.Deposit or TransactionType.Withdraw or TransactionType.Penalty)
+            .OrderBy(x => x.Date)
+            .ToList();
         
-        // Now loop through months and years etc
-        var dateFrom = Transactions.First().Date;
+        // Exit if no transactions at all
+        if (Transactions.Count == 0)
+            return;
+        
+        // Now get transactions we want to loop through
         dateTo ??= Transactions.Last().Date;
+        var transactionsToCalculate = Transactions;
+        transactionsToCalculate = transactionsToCalculate.Where(x => x.Date <= dateTo.Value).ToList();
         
-        var currentDate = dateFrom;
+        // Exit if no transactions for selected period
+        if (transactionsToCalculate.Count == 0)
+            return;
         
-        double totalBalance = 0;
-        double totalInterestForYear = 0;
+        // Now initialise essential variables
+        var openingDate = Transactions.First().Date;
+        var currentDate = openingDate;
+        var dateLastInterestPaid = openingDate;
+        var dateInterestLastCompounded = openingDate;
+        
+        // Variables representing accounts balance
+        double balanceToCalculateInterestOn = 0;
+        double interestWaitingToCompound = 0;
+        double amountToCalculateBonusOn = 0;
 
-        BonusPeriod currentBonusPeriod = GetBonusPeriodForDate(currentDate);
-
-        while (currentDate < dateTo)
+        while (currentDate <= dateTo)
         {
-            var transactionsForMonth = Transactions
-                .Where(x => x.Date.Month == currentDate.Month && x.Date.Year == currentDate.Year)
-                .OrderBy(x => x.Date)
+            // Get transactions for the current date
+            var transactionsForDate = Transactions
+                .Where(x => x.Date == currentDate)
                 .ToList();
-
-            var depositsForMonth = transactionsForMonth.Where(x => x.Type is 
-                TransactionType.Deposit or
-                TransactionType.GovernmentISABenefit
-            ).Sum(x => x.Amount);
             
-            var withdrawalsForMonth = transactionsForMonth.Where(x => x.Type is 
-                TransactionType.Withdraw or 
-                TransactionType.Penalty
-            ).Sum(x => x.Amount);
-
-            var balanceForMonth = depositsForMonth - withdrawalsForMonth;
-            var interestForMonth = (totalBalance + balanceForMonth) * ((_annualEquivalentRateAsPercentage / 100) / 12);
-
-            totalBalance += balanceForMonth;
-            totalInterestForYear += interestForMonth;
+            // Get balance for the current date, excluding interest
+            var depositsForPeriod = transactionsForDate
+                .Where(x => x.Type is TransactionType.Deposit)
+                .Sum(x => x.Amount);
             
-            Transactions.Add(new Transaction
+            var bonusForPeriod = transactionsForDate
+                .Where(x => x.Type is TransactionType.GovernmentISABenefit)
+                .Sum(x => x.Amount);
+            
+            var withdrawalsForPeriod  = transactionsForDate
+                .Where(x => x.Type is TransactionType.Withdraw or TransactionType.Penalty)
+                .Sum(x => x.Amount);
+
+            amountToCalculateBonusOn += depositsForPeriod;
+            
+            var balanceForPeriod = depositsForPeriod + bonusForPeriod - withdrawalsForPeriod;
+            balanceToCalculateInterestOn += balanceForPeriod;
+            
+            // Find out whether or not interest should be paid today
+            var payInterestToday = InterestAndTaxHelper.PayInterestToday(_interestPaidType, dateLastInterestPaid, currentDate);
+            if (payInterestToday)
             {
-                Type = TransactionType.Interest,
-                Date = new DateOnly(currentDate.Year, currentDate.Month, 28),
-                Amount = interestForMonth
-            });
-            
-            var newDate = currentDate.AddMonths(1);
-            
-            // If in a new bonus period, add government bonus for the previous periods transactions
-            BonusPeriod newBonusPeriod = GetBonusPeriodForDate(currentDate);
-            if (currentBonusPeriod.StartOfBonusPeriod.Month != newBonusPeriod.StartOfBonusPeriod.Month)
-            {
-                var transactionsForBonusPeriod = Transactions
-                    .Where(x => x.Date >= currentBonusPeriod.StartOfBonusPeriod && x.Date <= currentBonusPeriod.EndOfBonusPeriod)
-                    .OrderBy(x => x.Date)
-                    .ToList(); 
-                
-                var depositsForBonusPeriod = transactionsForBonusPeriod.Where(x => x.Type is 
-                    TransactionType.Deposit
-                ).Sum(x => x.Amount);
-                var withdrawalsForBonusPeriod = transactionsForBonusPeriod.Where(x => x.Type == TransactionType.Withdraw).Sum(x => x.Amount);
-
-                var balanceForBonusPeriod = depositsForBonusPeriod - withdrawalsForBonusPeriod;
-                var bonus = balanceForBonusPeriod * 0.25;
-                if (bonus > 1000)
-                    bonus = 1000;
+                // Calculate interest to be paid and add it to the account
+                dateLastInterestPaid = currentDate;
+                var interestToAdd = balanceToCalculateInterestOn * _actualInterestRate;
+                interestWaitingToCompound += interestToAdd;
                 
                 Transactions.Add(new Transaction
                 {
-                    Type = TransactionType.GovernmentISABenefit,
-                    Amount = bonus,
-                    Date = new DateOnly(currentBonusPeriod.EndOfBonusPeriod.Year, currentBonusPeriod.EndOfBonusPeriod.Month, 28)
+                    Date = currentDate,
+                    Amount = interestToAdd,
+                    Type = TransactionType.Interest
                 });
-                currentBonusPeriod = newBonusPeriod;
-                totalBalance += bonus;
             }
             
-            // If account birthday, pay interest into balance so that it can be compounded
-            if (newDate.Year == payInterestOn.Year && newDate.Month == payInterestOn.Month)
+            // Find out whether or not to compound interest today
+            if (InterestAndTaxHelper.CompoundInterestToday(_compoundType, dateInterestLastCompounded, currentDate))
             {
-                totalBalance += totalInterestForYear;
-                totalInterestForYear = 0;
-                payInterestOn = payInterestOn.AddYears(1);
+                balanceToCalculateInterestOn += interestWaitingToCompound;
+                interestWaitingToCompound = 0;
+                dateInterestLastCompounded = currentDate;
             }
-            currentDate = newDate;
-;        }
-
+            
+            // Now we do government bonus calculations
+            var bonusPeriod = GetBonusPeriodForDate(currentDate);
+            
+            if (currentDate == bonusPeriod.EndOfBonusPeriod)
+            {
+                var calculatedBonus = amountToCalculateBonusOn * 0.25;
+                Transactions.Add(new Transaction
+                {
+                    Type = TransactionType.GovernmentISABenefit,
+                    Amount = calculatedBonus,
+                    Date = new DateOnly(bonusPeriod.EndOfBonusPeriod.Year, bonusPeriod.EndOfBonusPeriod.Month, 28)
+                });
+                
+                amountToCalculateBonusOn = 0;
+            }
+            
+            currentDate = currentDate.AddDays(1);
+        }
+        
         // Finally add new interest and benefits transactions
-        Transactions = Transactions.OrderBy(x => x.Date).ToList();
-    } 
+        Transactions = Transactions
+            .OrderBy(x => x.Date)
+            .ToList();
+    }
     
     public override BaseSavingsAccount Deposit(DateOnly date, double amount)
     {
